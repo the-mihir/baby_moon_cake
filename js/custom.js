1fe2294e51a605c6b8fe6a431580ad8c5cc48f93
Mihir Das <58401556+the-mihir@users.noreply.github.com>
2021-07-26 21:33:35 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    "use strict";
+
+    // Parallax Js
+    $('#mylogoparallax').parallax({width: 1920,  height: 890, focusZ:20, sensitivityY:0.1, sensitivityX:0.05}); 
+
+
+
+});
+
